b620ce520d98337a6446d1962fe7abb29dc46913
Mahesh <mahesh@mahesh.ws>
2017-10-18 22:32:10 +0530

    Updated Currency Information.

--- src/main/java/ws/mahesh/api/exchangerates/model/Currency.java
+++ src/main/java/ws/mahesh/api/exchangerates/model/Currency.java
@@ -10,13 +10,19 @@ package ws.mahesh.api.exchangerates.model;
 /**
  * Enum for Currencies.
  *
- * @author Mahesh
+ * @author Mahesh.
  */
 public enum Currency {
-    EUR("Euro", "", ""), AUD("", "", ""), BGN("", "", ""), BRL("", "", ""), CAD("", "", ""), CHF("", "", ""), CNY("", "", ""), CZK("", "", ""), DKK("", "", ""),
-    GBP("", "", ""), HKD("", "", ""), HRK("", "", ""), HUF("", "", ""), IDR("", "", ""), ILS("", "", ""), INR("Indian Rupee", "India", ""), JPY("", "", ""),
-    KRW("", "", ""), MXN("", "", ""), MYR("", "", ""), NOK("", "", ""), NZD("", "", ""), PHP("", "", ""), PLN("", "", ""), RON("", "", ""), RUB("", "", ""),
-    SEK("", "", ""), SGD("", "", ""), THB("", "", ""), TRY("", "", ""), USD("US Dollar", "United States Of America", ""), ZAR("", "", "");
+    EUR("Euro", "Euro Member Countries", "\u20AC"), AUD("Australian Dollar", "Australia", ""), BGN("Bulgarian Lev", "Bulgaria", ""),
+    BRL("Brazilian Real", "Brazil", ""), CAD("Canadian Dollar", "Canada", ""), CHF("Swiss Franc", "Switzerland", ""), CNY("Chinese Yuan Renminbi", "China", ""),
+    CZK("Czech Koruna", "Czechia", ""), DKK("Danish Krone", "Denmark", ""), GBP("British Pound", "United Kingdom", ""),
+    HKD("Hong Kong Dollar", "Hong Kong", ""), HRK("Croatian Kuna", "Croatia", ""), HUF("Hungarian Forint", "Hungary", ""),
+    IDR(" Indonesian Rupiah", " Indonesia", ""), ILS("Israeli Shekel", " Israel", ""), INR("Indian Rupee", "India", "\u20B9"),
+    JPY("Japanese Yen", "Japan", "\u00A5"), KRW("South Korean Won", "Korea (South)", ""), MXN("Mexican Peso", "Mexico", ""),
+    MYR("Malaysian Ringgit", "Malaysia", ""), NOK("Norwegian Krone", "Norway", ""), NZD("New Zealand Dollar", "New Zealand", ""),
+    PHP("Philippine Peso", "Philippines", ""), PLN("Polish Zloty", "Poland", ""), RON("Romanian Leu", "Romania", ""), RUB("Russian Ruble", "Russia", ""),
+    SEK("Swedish Krona", "Sweden", ""), SGD("Singapore Dollar", "Singapore", ""), THB("Thai Baht", "Thailand", "\u0E3F"),
+    TRY("Turkish Lira", "Turkey", "\u20BA"), USD("US Dollar", "United States", "\u0024"), ZAR("South African Rand", "South Africa", "");
 
 
     private String name;
